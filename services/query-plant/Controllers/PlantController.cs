20e198562404a2f06ba70286f9d04a3af6ca7633
Paul Glavich <glav@theglavs.com>
2021-05-31 15:21:49 +1000

    Initial default api scaffold

---
+++ services/query-plant/Controllers/PlantController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace query_plant.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlantController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Mint", "Chilli", "Rosemary", "Spring onions", "Tomatoes", "Spinach", "Sage", "Coriander"
+        };
+
+        private readonly ILogger<PlantController> _logger;
+
+        public PlantController(ILogger<PlantController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<PlantSummary> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new PlantSummary
+            {
+                Date = DateTime.Now.AddDays(index),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ services/query-plant/PlantSummary.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace query_plant
+{
+    public class PlantSummary
+    {
+        public DateTime Date { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
