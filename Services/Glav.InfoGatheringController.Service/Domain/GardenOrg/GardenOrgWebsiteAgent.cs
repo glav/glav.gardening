4b18aad71cc25ce79c7adbbf6d866c2567a57e2d
Paul Glavich <glav@theglavs.com>
2021-07-09 09:08:11 +1000

    Some minor changes to support DAPR

--- Services/Glav.InfoGatheringController.Service/Domain/GardenOrg/GardenOrgWebsiteAgent.cs
+++ Services/Glav.InfoGatheringController.Service/Domain/GardenOrg/GardenOrgWebsiteAgent.cs
@@ -5,17 +5,16 @@ namespace Glav.InformationGathering.Domain.GardenOrg
 {
     public class GaardenOrgWebsiteAgent : IInfoGatheringAgent
     {
+        private int _progress = 0;
         const string queryUrl = "https://garden.org/plants/search/text/?q={0}";
 
-        public Task<int> Progress()
-        {
-            throw new System.NotImplementedException();
-        }
+        public int Progress => _progress;
 
         public async Task StartAsync(string queryTerm)
         {
             var client = new HttpClient();
             var result = await client.GetStringAsync(string.Format(queryUrl,queryTerm));
+            _progress = 33;
         }
 
         public Task StopAsync()
